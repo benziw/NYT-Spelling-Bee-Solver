d2454699ec199d1c1042bd5a16683c81809dcae0
Ben <benweltsch99@gmail.com>
2022-08-11 08:05:01 -0700

    initial commit

---
+++ src/components/Hive.js
@@ -0,0 +1,14 @@
+import '../css/Hive.css';
+import Hex from "./Hex";
+
+export default function Hive(){
+
+  let letters = ['i','o','z','e','n','m','d'];
+
+  return (
+    <div class='hive'>
+      <Hex letter='o' color='#f7da21' svgNum='0'/>
+    </div>
+    
+  );
+}
