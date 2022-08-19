6c268acc5de0298d3c945898bd4f2207216417ba
Ben <benweltsch99@gmail.com>
2022-08-19 14:17:00 -0700

    better styling and formatting, better wordlist behavior

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import Axios from 'axios';
+import '../css/WordList.css';
 
 import Button from '@mui/material/Button';
 import LoadingButton from '@mui/lab/LoadingButton';
+
+function strToUniqueSorted(str) {
+  return Array.from(new Set(str.split(''))).sort().join('');
+}
 
 export default function WordList(props) {
 
@@ -10,6 +15,8 @@ export default function WordList(props) {
   const [realWords, setRealWords] = useState([]);
   const letters = props.letters.replaceAll("_", "").split('').join('');
   const sortedLetters = props.letters.replaceAll("_", "").split('').sort().join('');
+  const [pangramLetters, setPangramLetters] = useState(letters);
+  const [noWords, setNoWords] = useState(false);
 
 
   const getWordsOnClick = (e) => {
@@ -25,41 +32,48 @@ export default function WordList(props) {
 
     const doRest = async function () {
       const words = await getWordsPromise;
-      // console.log(words);
-      // let matchedWords = words[letters].map(w => w.toUpperCase());
-      // console.log(matchedWords);
-      // console.log(letters);
-      // console.log(letters.charAt(0))
-      // let newMatchedWords = [];
-      // for(let i = 0; i < matchedWords.length; i++){
-      //   if(matchedWords[i].includes(letters.charAt(0))){
-      //     newMatchedWords += matchedWords[i]
-      //   }
-      // }
-      // console.log(matchedWords);
-      let matchedWords = words[sortedLetters].filter(wrd => wrd.includes(letters.charAt(0)));
-      console.log(matchedWords);
-     
 
-      setRealWords(matchedWords || [`no words found from letters '${letters.toUpperCase()}'`]);
+      if (!words[sortedLetters]) {
+        setRealWords(['']);
+        setNoWords(true);
+        return null;
+      }
+      else {
+        let matchedWords = words[sortedLetters].filter(wrd => wrd.includes(letters.charAt(0)));
+        console.log(matchedWords);
+
+        setRealWords(matchedWords || [`no words found from letters '${letters.toUpperCase()}'`]);
+      }
     }
 
-    setRealWords(['loading...']);
+    setRealWords(['\n']);
+    setNoWords(false);
     doRest();
+    setPangramLetters(letters);
   }
 
   return (
     <div className="wordListContainer">
-      
-      {letters.length == 7 ?
-        (realWords[0] !== 'loading...' ? 
+
+      {letters.length === 7 ?
+        (realWords[0] !== '\n' ?
           <Button onClick={getWordsOnClick} variant='contained'>get words</Button> :
-          <LoadingButton loading variant="outlined">loading...</LoadingButton>) :
+          <Button variant="outlined">loading...</Button>) :
         <Button onClick={getWordsOnClick} variant='contained' disabled>get words</Button>
       }
 
+      {noWords ?
+        <h4>{`No words found from '${letters.toUpperCase()}'`}</h4> :
+        null
+      }
+
       {realWords.map((word) => (
-        <p key={word}>{word.toUpperCase()}</p>
+
+        <div className='wordContainer'>
+          <p className='pWord' key={word}>{word.toUpperCase()}</p>
+          <p className='pPangram'>{strToUniqueSorted(pangramLetters) === strToUniqueSorted(word) ? 'Pangram!' : ''}</p>
+        </div>
+
       ))}
 
     </div>
