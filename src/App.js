96f2d634f74783a6f84fd6090802e9ed65759eff
Ben <benweltsch99@gmail.com>
2022-08-21 17:21:43 -0700

    styling, going to experiment in branch 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Hive from './components/Hive';
 import WordList from './components/WordList';
 
 import TextField from '@mui/material/TextField';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HiveIcon from '@mui/icons-material/Hive';
+import Link from '@mui/material/Link';
 
 
 export default function App() {
@@ -13,25 +16,40 @@ export default function App() {
   const [letters, setLetters] = useState('_______');
 
   const textOnChange = (e) => {
-    let str = e.target.value.replace(/[^a-z]/gi, '');
-    let strLower = str.toLowerCase()
-    if (strLower.indexOf(strLower[strLower.length-1]) !== strLower.length-1){
-      str = str.slice(0,-1);
+
+    let str = e.target.value.replace(/[^a-zA-Z]/gi, '');
+
+    if (str.length >= 7) {
+      str = str.slice(0, 7);
+    }
+    else {   //remove repeats
+      let newChar = str.slice(-1);
+      let oldStr = str.slice(0, -1).toLowerCase();
+      //console.log(`newChar: ${newChar} | oldStr: ${oldStr}`);
+      str = (oldStr.indexOf(newChar) > -1 ? oldStr : str);
+      //console.log(`str: ${str}`);
     }
 
-    while(str.length < 7){
+    while (str.length < 7) {
       str += "_";
     }
-    setLetters(str); 
+    setLetters(str);
   }
 
   return (
     <div className="App">
 
-      <div className='header'></div>
+      <div className='header'>
+        <HiveIcon />
+        <h1>nyt spelling bee solver</h1>
+        <Link href='https://github.com/benziw/NYT-Spelling-Bee-Solver' target="_blank">
+          <GitHubIcon />
+        </Link>
+
+      </div>
 
       <div className='inputContainer'>
-        <TextField onChange={textOnChange} maxLength='7' value={letters.replaceAll("_", "").toUpperCase()}></TextField>
+        <TextField autoFocus={true} onChange={textOnChange} maxLength='7' value={letters.replaceAll("_", "").toUpperCase()}></TextField>
         <h4>{`Enter letters above. First letter will be center letter. No duplicates.`}</h4>
       </div>
 
--- src/components/WordList.js
+++ src/components/WordList.js
@@ -3,20 +3,28 @@ import Axios from 'axios';
 import '../css/WordList.css';
 
 import Button from '@mui/material/Button';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { TextField } from '@mui/material';
+
 
 function strToUniqueSorted(str) {
   return Array.from(new Set(str.split(''))).sort().join('');
+}
+
+function isPangram(str) {
+  return strToUniqueSorted(str) === strToUniqueSorted(str);
 }
 
 export default function WordList(props) {
 
   //const [sevenLetters, setSevenLetters] = useState(false);
   const [realWords, setRealWords] = useState([]);
+  const [noWords, setNoWords] = useState(false);
+  const [displayString, setDisplayString] = useState('');
+
   const letters = props.letters.replaceAll("_", "").split('').join('');
   const sortedLetters = props.letters.replaceAll("_", "").split('').sort().join('');
-  const [pangramLetters, setPangramLetters] = useState(letters);
-  const [noWords, setNoWords] = useState(false);
+
+;
 
 
   const getWordsOnClick = (e) => {
@@ -34,22 +42,23 @@ export default function WordList(props) {
       const words = await getWordsPromise;
 
       if (!words[sortedLetters]) {
-        setRealWords(['']);
+        setRealWords(['no words found']);
         setNoWords(true);
+        //setDisplayString('no words found');
         return null;
       }
       else {
         let matchedWords = words[sortedLetters].filter(wrd => wrd.includes(letters.charAt(0)));
         console.log(matchedWords);
 
-        setRealWords(matchedWords || [`no words found from letters '${letters.toUpperCase()}'`]);
+        setRealWords(matchedWords);
       }
     }
 
     setRealWords(['\n']);
     setNoWords(false);
     doRest();
-    setPangramLetters(letters);
+    
   }
 
   return (
@@ -62,19 +71,11 @@ export default function WordList(props) {
         <Button onClick={getWordsOnClick} variant='contained' disabled>get words</Button>
       }
 
-      {noWords ?
-        <h4>{`No words found from '${letters.toUpperCase()}'`}</h4> :
-        null
-      }
-
-      {realWords.map((word) => (
-
-        <div className='wordContainer'>
-          <p className='pWord' key={word}>{word.toUpperCase()}</p>
-          <p className='pPangram'>{strToUniqueSorted(pangramLetters) === strToUniqueSorted(word) ? 'Pangram!' : ''}</p>
-        </div>
-
-      ))}
+      <TextField multiline minRows={12} maxRows={12} 
+        value={realWords.join('\n')}
+        className='wordsField'
+        fullWidth={true}
+      />
 
     </div>
   )
